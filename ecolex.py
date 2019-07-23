949193469136998e3455132486f744f1fd78684d
Alex Morega <alex@grep.ro>
2019-07-23 10:00:34 +0300

    Prettify the code

--- ecolex.py
+++ ecolex.py
@@ -6,15 +6,21 @@ from jinja2 import Environment, FileSystemLoader
 import requests
 
 path = Path(__file__).parent.resolve()
+
 config = ConfigParser()
 config.optionxform = str
 config.read(path / 'ecolex.ini')
-jinja_env = Environment(
-    variable_start_string="${",
-    variable_end_string="}",
+nomad = config.get('cluster', 'nomad')
+
+jinja = Environment(
+    variable_start_string='${',
+    variable_end_string='}',
     loader=FileSystemLoader(str(path / 'templates')),
 )
-nomad = config.get('cluster', 'nomad')
+
+
+def render(filename, **kwargs):
+    return jinja.get_template(filename).render(**kwargs)
 
 
 class Options:
@@ -23,16 +29,9 @@ class Options:
     fixtures = path / 'fixtures'
 
 
-def render(filename, **kwargs):
-    return jinja_env.get_template(filename).render(**kwargs)
-
-
 def deploy():
     hcl = render('ecolex.nomad', options=Options())
-    spec = requests.post(f'{nomad}/v1/jobs/parse', json={
-        'JobHCL': hcl,
-        'Canonicalize': True,
-    }).json()
+    spec = requests.post(f'{nomad}/v1/jobs/parse', json={'JobHCL': hcl}).json()
     result = requests.post(f'{nomad}/v1/jobs', json={'job': spec}).json()
     print(result)
 
