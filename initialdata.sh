5f1b65adc9bafa627f725ee4da5ea3bc30e65e46
Alex Morega <alex@grep.ro>
2019-07-29 16:15:04 +0300

    Local paths

--- initialdata.sh
+++ initialdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-manage.py import treaty
-manage.py import literature
-manage.py import decision
-manage.py import court_decision
+./manage.py import treaty
+./manage.py import literature
+./manage.py import decision
+./manage.py import court_decision
