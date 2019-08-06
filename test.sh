aa34c0cc38c207f2a90376826f8ebc39e0a5deb7
Alex Morega <alex@grep.ro>
2019-08-06 14:53:26 +0300

    Retry homepage for 5 minutes

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -ex
 
-curl http://10.66.60.1:8000
+
+homepage=http://10.66.60.1:8000
+attempts=60
+
+function retry_get_homepage() {
+  for i in $(seq 1 $attempts); do
+    if curl -f "$homepage"; then return 0; fi
+    sleep 5
+  done
+  echo "Homepage failed after $attempts attempts"
+  exit 1
+}
+
+retry_get_homepage
